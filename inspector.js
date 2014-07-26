db09ad2b7084683a3c6c57bf2b57eb99f5b8e74c
HPNeo <hpneo@hotmail.com>
2014-07-25 23:04:46 -0500

    Clear DOM elements from circular references before logging them

--- inspector.js
+++ inspector.js
@@ -108,6 +108,126 @@ function mapDOM(element) {
     mapDOM(element.children[i]);
   }
 };
+
+function elementToSelector(element) {
+  var selector = element.tagName.toLowerCase();
+
+  if (element.id !== '') {
+    selector += '#' + element.id
+  }
+
+  if (element.className !== '') {
+    selector += '#' + element.className.replace(/ /g, '.');
+  }
+
+  return selector;
+}
+
+function objectKeys(object) {
+  if (Object.keys) {
+    return Object.keys(object);
+  }
+  else {
+    var keys = [];
+
+    for (var i in object) {
+      keys.push(i);
+    }
+
+    return keys;
+  }
+}
+
+function preJSON(object) {
+  var linearObject;
+
+  if (
+    (typeof object === 'string') ||
+    (typeof object === 'number') ||
+    (typeof object === 'boolean')
+  ) {
+    linearObject = object;
+  }
+  else if (
+    (object instanceof String) ||
+    (object instanceof Number) ||
+    (object instanceof Boolean)
+  ) {
+    linearObject = object.constructor(object);
+  }
+  else if (typeof object === 'undefined') {
+    linearObject = 'undefined';
+  }
+  else if (object === null) {
+    linearObject = null;
+  }
+  else if (object instanceof Function) {
+    linearObject = object.toString();
+  }
+  else if (object instanceof Date) {
+    linearObject = object;
+  }
+  else if (object instanceof Document) {
+    linearObject = 'document';
+  }
+  else if (object instanceof Comment) {
+    linearObject = '<!--' + object.nodeValue + '-->';
+  }
+  else if (object instanceof Text) {
+    linearObject = object.nodeValue;
+  }
+  else if (object instanceof Attr) {
+    linearObject = {};
+    linearObject[object.name] = object.nodeValue;
+  }
+  else if (object instanceof HTMLElement) {
+    linearObject = {};
+    var keys = objectKeys(object);
+
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+
+      try {
+        value = object[key]
+      }catch(e) {
+        value = undefined;
+      }
+
+      if (value instanceof HTMLElement) {
+        value = elementToSelector(value);
+      }
+
+      linearObject[key] = preJSON(value);
+    }
+  }
+  else if (
+    (object instanceof Array) ||
+    (typeof object.length === 'number')
+  ) {
+    linearObject = [];
+
+    for (var i = 0; i < object.length; i++) {
+      var value = object[i];
+
+      if (value instanceof HTMLElement) {
+        value = elementToSelector(value);
+      }
+
+      linearObject.push(preJSON(value));
+    }
+  }
+  else if (object instanceof Object) {
+    linearObject = {};
+    var keys = objectKeys(object);
+
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      linearObject[key] = preJSON(object[key]);
+    }
+  }
+
+  return linearObject;
+};
 var Async = {
   request: function(request_type, options) {
     options = options || {};
