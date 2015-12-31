fe1e88c56ed48862b7add2945d0b5069f5b5ad28
HPNeo <hpneo@hotmail.com>
2015-12-31 17:30:44 -0500

    Update task

--- build/inspector.js
+++ build/inspector.js
@@ -525,7 +525,7 @@ function createIdentifier() {
 function extractSettings(options) {
   var baseIdentifier = options.identifier,
       computedIdentifier = createIdentifier(),
-      endpoint = (options.endpoint || "https://domscope.com/endpoint"),
+      endpoint = (options.endpoint || "http://domscope.com/endpoint"),
       alertOnError = (typeof options.alertOnError === 'boolean') ? options.alertOnError : false,
       settings = {
         'navigator': global.navigator.userAgent,
